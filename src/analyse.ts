70b37f4bc6bca39ce64fd97bfd94227bbb7f9d21
Ludovic L'Hours <ludovic.lhours@gmail.com>
2017-06-25 21:58:15 +0200

    Complete solution with recovery

--- src/analyse.ts
+++ src/analyse.ts
@@ -13,7 +13,12 @@ export interface IErrorReporter {
     (span: Span, message: string): void;
 }
 
-enum TokenType {
+export enum TokenType {
+    IDENT,
+    STRING_LITERAL,
+    AND, OR, IS, NULL, NOW,
+    EQUALS, LOWER,
+    LPAR, RPAR,
     EOF
 }
 
@@ -23,13 +28,195 @@ export interface Token {
     value?: string;
 }
 
-export class Lexer {
-    private index = 0;
+const KEYWORDS = {
+    "OR": TokenType.OR,
+    "AND": TokenType.AND,
+    "IS": TokenType.IS,
+    "NULL": TokenType.NULL,
+    "NOW": TokenType.NOW
+};
 
-    constructor(private content: String, private reporter: IErrorReporter) {}
+function mergeSpan(spans: Span[]): Span {
+    function minPosition(a: Position, b: Position): Position {
+        if (a.line < b.line) {
+            return a;
+        }
+        if (a.line == b.line) {
+            return { line: a.line, column: Math.min(a.column, b.column)};
+        }
+        return b;
+    }
+    function maxPosition(a: Position, b: Position): Position {
+        if (a.line > b.line) {
+            return a;
+        }
+        if (a.line == b.line) {
+            return { line: a.line, column: Math.max(a.column, b.column)};
+        }
+        return b;
+    }
+    function merge(a: Span, b: Span): Span {
+        return {from: minPosition(a.from, b.from), to: maxPosition(a.to, b.to)};
+    }
+    let span: Span = null;
+    spans.forEach(s => {
+        if (span) {
+            span = merge(span, s)
+        } else {
+            span = s;
+        }
+    })
+    return span;
+} 
+
+export class Lexer {
+    private position = 0;
+    private line = 0;
+    private column = 0;
+    private startLine: number;
+    private startColumn: number;
+    private previousColumn: number;
+    private badCharsSpan: Span = null;
+    private badChars = "";
+
+    constructor(private content: string, private reporter: IErrorReporter) {}
 
     public nextToken(): Token {
-        throw new Error("not complete");
+        while (true) {
+            this.startToken()
+            let c = this.nextChar();
+            if (c == '') {
+                return this.token(TokenType.EOF);
+            }
+            if (c == '(') {
+                return this.token(TokenType.LPAR);
+            }
+            if (c == ')') {
+                return this.token(TokenType.RPAR);
+            }
+            if (c == '=') {
+                return this.token(TokenType.EQUALS);
+            }
+            if (c == '<') {
+                return this.token(TokenType.LOWER);
+            }
+            if (c == '\'') {
+                return this.stringLiteral(c);
+            }
+            if (this.isLetter(c)) {
+                return this.ident(c);
+            }
+            if (!this.isSpace(c)) {
+                if (this.badChars == "") {
+                    this.badCharsSpan = this.makeSpan();
+                } else {
+                    this.badCharsSpan = mergeSpan([this.badCharsSpan, this.makeSpan()]);
+                }
+                this.badChars += c;
+            }
+        }
+    }
+
+    private flushBadChars() {
+        if (this.badChars.length > 0) {
+            let msg = "unexpected char '" + this.badChars + "'";
+            this.reporter(this.badCharsSpan, msg);
+            this.badChars = "";    
+        }
+    }
+
+    private ident(c: string) {
+        let value = "";
+        do {
+            value += c;
+            c = this.nextChar();
+        } while (this.isLetterOrDigit(c))
+        this.pushback(c);
+        let keyword = KEYWORDS[value];
+        if (keyword) {
+           return this.token(keyword); 
+        }
+        return this.token(TokenType.IDENT, value);
+    }
+
+    private stringLiteral(separator: string) {
+        let value = "";
+        while (true) {
+            let c = this.nextChar();
+            if (c == separator) {
+                break;
+            }
+            if (c == "" || c == "\n") {
+                this.pushback(c);
+                this.reporter(this.makeSpan(), "unterminated string");
+                break;
+            }
+            value += c;
+        }
+        return this.token(TokenType.STRING_LITERAL, value);
+    }
+
+    private isLetterOrDigit(c: string) {
+        return this.isLetter(c) || this.isDigit(c);
+    }
+
+    private isLetter(c: string) {
+        return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+    }
+
+    private isDigit(c: string) {
+        return c >= '0' && c <= '9';
+    }
+
+    private isSpace(c: string) {
+        return c == ' '|| c == '\t' || c == '\n';
+    }
+
+    private token(type: TokenType, value: string = null): Token {
+        this.flushBadChars();
+        if (value) {
+            return { span: this.makeSpan(), type: type, value: value };
+        }
+        return { span: this.makeSpan(), type: type };
+    }
+
+    private startToken() {
+        this.startLine = this.line;
+        this.startColumn = this.column;
+    }
+
+    private makeSpan(): Span {
+        return {
+            from: { line: this.startLine, column: this.startColumn },
+            to:   { line: this.line, column: this.column }
+        };
+    }
+
+    private pushback(c) {
+        if (c.length > 0) {
+            --this.position;
+            if (c == '\n') {
+                --this.line;
+                this.column = this.previousColumn;
+            } else {
+                --this.column;
+            }
+        }
+    }
+
+    private nextChar(): string {
+        if (this.position == this.content.length) {
+            return "";
+        }
+        let c = this.content.charAt(this.position++);
+        if (c == '\n') {
+            this.previousColumn = this.column;
+            this.line++;
+            this.column = 0;
+        } else {
+            this.column++;
+        }
+        return c;
     }
 }
 
@@ -40,12 +227,173 @@ export class Parser {
 
     constructor(private lexer: Lexer, private reporter: IErrorReporter) {}
 
-    public parseQuery(): any {
+    public parseQuery(): ast.Query {
         this.nextToken();
-        throw new Error("not complete");
+        let query = this.parseOr();
+        this.expect(TokenType.EOF, false);
+        return query;
+    }
+
+    /**
+     * Or ::= And ( OR And )*
+     * 
+     * Follow(Or) = { EOF, RPAR }
+     */
+    private parseOr() {
+        let elements = [ this.parseAnd() ];
+        while (this.testAndSkip(TokenType.OR)) {
+            elements.push(this.parseAnd());
+        }
+        if (elements.length == 1) {
+            return elements[0];
+        }
+        return ast.Builder.OrQuery(this.nodesSpan(elements), elements);
+    }
+
+    /**
+     * And ::= Unitary ( AND Unitary )*
+     * 
+     * First(And)  = First(Unitary) = { LPAR, IDENT }
+     * Follow(And) = { OR } + Follow(Or) = { OR, RPAR, EOF }
+     */
+    private parseAnd() {
+        let elements = [ this.parseUnitary() ];
+        while (true) {
+            if (this.testAndSkip(TokenType.AND)) {
+                elements.push(this.parseUnitary());
+            } else if (this.testAny([TokenType.IDENT, TokenType.LPAR])) {
+                this.reportExpect(TokenType.AND); // or maybe { OR, RPAR, EOF } ?
+                elements.push(this.parseUnitary());
+            } else {
+                break;
+            }
+        }
+        if (elements.length == 1) {
+            return elements[0];
+        }
+        return ast.Builder.AndQuery(this.nodesSpan(elements), elements);
+    }
+
+    /**
+     * Unitary ::= LPAR Or RPAR
+     * Unitary ::= Ident ( EQUALS Value | LOWER NOW | IS NULL)
+     * 
+     * Follow(Unitary) = { AND } + Follow(And) = { AND, OR, RPAR, EOF }
+     */
+    private parseUnitary() {
+        let start = this.token.span;
+        if (this.testAndSkip(TokenType.LPAR)) {
+            let query = this.parseOr();
+            let end = this.expect(TokenType.RPAR, false);
+            let endSpan = end != null ? end.span : query.span;
+            return ast.Builder.GroupQuery(mergeSpan([start, endSpan]), query);
+        }
+        return this.recoverWith([TokenType.AND, TokenType.OR, TokenType.RPAR, TokenType.EOF], (span) => {
+            return ast.Builder.BadUnitary(mergeSpan([start, span ]));
+        }, () => {
+            let ident = this.parseIdent();
+            return this.recoverWith([TokenType.AND, TokenType.OR, TokenType.RPAR, TokenType.EOF], (span) => {
+                return ast.Builder.BadMatch(mergeSpan([start, span ]), ident);
+            }, () => {
+                if (this.testAndSkip(TokenType.EQUALS)) {
+                    let value = this.parseValue();
+                    return ast.Builder.EqualsMatch(this.nodesSpan([ident, value]), ident, value);
+                }
+                if (this.testAndSkip(TokenType.LOWER)) {
+                    let t = this.expect(TokenType.NOW);
+                    return ast.Builder.BeforeTodayMatch(mergeSpan([ident.span, t.span]), ident);
+                }
+                if (this.testAndSkip(TokenType.IS)) {
+                    let t = this.expect(TokenType.NULL);
+                    return ast.Builder.IsNullMatch(mergeSpan([ident.span, t.span]), ident);
+                }
+                this.report("expecting token =, < or IS");
+            });
+        });
+        
+    }
+
+    /**
+     * Ident ::= IDENT
+     * Follow(Ident) = { EQUALS, LOWER, IS }
+     */
+    private parseIdent(): ast.Ident {
+        let t = this.expect(TokenType.IDENT);
+        return ast.Builder.Ident(t.span, t.value);
+    }
+
+    /**
+     * Value ::= STRING_LITERAL
+     * Follow(Value) = Follow(Unitary) = { AND, OR, RPAR, EOF }
+     */
+    private parseValue(): ast.Value {
+        let t = this.expect(TokenType.STRING_LITERAL);
+        return ast.Builder.StringValue(t.span, t.value);
+    }
+
+    private testAndSkip(type: TokenType): boolean {
+        if (this.token.type == type) {
+            this.nextToken();
+            return true;
+        }
+        return false;
+    }
+
+    private testAny(types: TokenType[]): boolean {
+        return types.indexOf(this.token.type) >= 0;
+    }
+
+
+    private recoverWith<T>(syncTokens: TokenType[], makeError: (Span) => T, parseFunc: () => T): T {
+        let start = { from: this.token.span.from, to: this.token.span.from };
+        try {
+            return parseFunc();
+        } catch(e) {
+            let tokens = this.skipTo(syncTokens);
+            let range = tokens.length > 0 ? mergeSpan([ start, tokens[tokens.length-1].span ]) : start;
+            return makeError(range);
+        }
+    }
+
+    private skipTo(syncTokens: TokenType[]): Token[] {
+        let tokens: Token[] = []
+        while (! (this.token.type == TokenType.EOF || syncTokens.indexOf(this.token.type) >= 0)) {
+            tokens.push(this.token);
+            this.nextToken();
+        }
+        return tokens;
+    }
+
+    private expect(type: TokenType, doThrow: boolean = true): Token {
+        if (this.token.type != type) {
+            this.reportExpect(type, doThrow);
+            return null;
+        } else {
+            let t = this.token;
+            this.nextToken();
+            return t;
+        }
+    }
+
+    private reportExpect(type: TokenType, doThrow: boolean = false) {
+        let msg = "expecting token " + TokenType[type];
+        this.report(msg, doThrow);
+    }
+
+    private report(msg: string, doThrow: boolean = true) {
+        this.reporter(this.token.span, msg);
+        if (doThrow) {
+            throw new Error(msg
+                + " at line " + this.token.span.from.line
+                + " column " + this.token.span.from.column);
+        }
     }
 
     private nextToken() {
         this.token = this.lexer.nextToken();
+    }
+
+    private nodesSpan(nodes: ast.AstNode[]) {
+        return mergeSpan(nodes.map(node => node.span));
     }
 }
